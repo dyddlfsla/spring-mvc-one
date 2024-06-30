3028c91627a5273ddc80dcdbb43879071e3bf730
dyddlfsla <7942dyddlfsl@naver.com>
2024-06-30 23:24:58 +0900

    Initial commit

---
+++ src/main/java/com/pamph/spring/SpringMvcOneApplication.java
@@ -0,0 +1,13 @@
+package com.pamph.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pamph/spring/SpringMvcOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pamph.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
