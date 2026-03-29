916923d45ce7b6e1a53235724600b1204dbc7bd0
dyddlfsla <7942dyddlfsla@gmail.com>
2026-03-29 23:41:36 +0900

    add comment

--- src/main/java/com/pamph/spring/note/$10_FormDataRequest.java
+++ src/main/java/com/pamph/spring/note/$10_FormDataRequest.java
@@ -35,6 +35,13 @@ public class $10_FormDataRequest {
   *
   * 정리하면 request.getParameter() 는 GET URL 쿼리 파라미터 형식도 지원하고, HTML Form 형식도 둘 다 지원한다.
   *
+  * ※참고
+  * ▶ content-type 은 HTTP 메시지 바디의 데이터 형식이 무엇인지 지정한다.
+  * ▶ GET URL 쿼리 파라미터 형식으로 데이터를 전달할 때는 HTTP 메시지 바디를 사용하지 않기 때문에 content-type 이 존재하지 않는다.
+  * ▶ HTML Form 태그 형식으로 데이터를 전달하면 HTTP 메시지 바디에 해당 데이터를 포함해서 보내기 때문에 바디에 포함된 데이터가 어떤 형식인지
+  *   꼭 content-type 을 지정해야 한다. 이렇게 form 태그를 활용해 데이터를 전송하는 형식을 application/x-www-form-urlencoded 라 한다.
+  *
+  *
   *
   *
   *
